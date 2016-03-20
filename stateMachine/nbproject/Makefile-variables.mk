d46d5648e40bd057746b6193ceebfbc6fd56c567
Danilo Graca <>
2016-03-20 01:56:08 -0300

    My first Commit

---
+++ stateMachine/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=None-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
+CND_ARTIFACT_NAME_Debug=statemachine
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/statemachine
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
+CND_PACKAGE_NAME_Debug=statemachine.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/statemachine.tar
+# Release configuration
+CND_PLATFORM_Release=None-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
+CND_ARTIFACT_NAME_Release=statemachine
+CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/statemachine
+CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
+CND_PACKAGE_NAME_Release=statemachine.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/statemachine.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
