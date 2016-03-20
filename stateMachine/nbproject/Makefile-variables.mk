d93be8dc4d7b3c186af9b6cc530750d3c230e30b
Homine <Homine@Homine08>
2016-03-20 12:57:42 -0300

    My first Commit

--- stateMachine/nbproject/Makefile-variables.mk
+++ stateMachine/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=None-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=statemachine
-CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/statemachine
-CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/statemachine
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=statemachine.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/statemachine.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/statemachine.tar
 # Release configuration
-CND_PLATFORM_Release=None-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=statemachine
-CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/statemachine
-CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/statemachine
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=statemachine.tar
-CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/statemachine.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/statemachine.tar
 #
 # include compiler specific variables
 #
